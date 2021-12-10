6cbff40da4d0c7c232dedae7962d1a81a2b467af
Aditya-Ojha <ojhaaditya576@gmail.com>
2021-12-10 23:41:45 +0530

    spinner updated

--- src/components/App.js
+++ src/components/App.js
@@ -31,46 +31,40 @@ const App = () => {
     }
 
 
-
     return (
-        <>
 
-
+        (!profiles) ? (
+            <div class="spinner">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
+            </div>
+        ) : (
             <div style={{ padding: '20px' }}>
                 <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32, xl: 36 }, 24]}>
                     {
-                        (profiles) ? (
-                            profiles.map((profile, id) => {
-                                return (
-                                    <Col key={id} className="gutter-row" xs={24} sm={12} md={8} lg={8} xl={6}>
-                                        <User
-                                            id={id}
-                                            name={profile.name}
-                                            email={profile.email}
-                                            phone={profile.phone}
-                                            website={profile.website}
-                                            username={profile.username}
-                                            deleteProfile={deleteProfile}
-                                        />
-                                    </Col>
-                                )
-                            })
-
-
-                        ) : (
-                            <div className="spinner">
-                                <div className="double-bounce1"></div>
-                                <div className="double-bounce2"></div>
-                            </div>
-                        )
+                        profiles.map((profile, id) => {
+                            return (
+                                <Col key={id} className="gutter-row" xs={24} sm={12} md={8} lg={8} xl={6}>
+                                    <User
+                                        id={id}
+                                        name={profile.name}
+                                        email={profile.email}
+                                        phone={profile.phone}
+                                        website={profile.website}
+                                        username={profile.username}
+                                        deleteProfile={deleteProfile}
+                                    />
+                                </Col>
+                            )
+                        })
                     }
-
-
                 </Row>
             </div>
-
-        </>
+        )
     )
 }
 
 export default App;
+
+
